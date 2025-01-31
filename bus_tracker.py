5c68757ed52c4fc4219d160b6baefe92bcbf39a8
Gopi Shankar Ravady <gopi2605@gmail.com>
2025-01-31 06:26:18 -0600

    changed from flask_ask to flask_ask_sdk

--- bus_tracker.py
+++ bus_tracker.py
@@ -1,10 +1,14 @@
-from flask import Flask, request
-from flask_ask import Ask, statement, session
+from flask import Flask, request, jsonify
+from flask_ask_sdk.skill_adapter import SkillAdapter
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.utils import is_request_type, is_intent_name
 import requests
 from math import radians, sin, cos, sqrt, atan2
+from word2number import w2n
 
 app = Flask(__name__)
-ask = Ask(app, "/")
+skill_builder = SkillBuilder()
 
 user_location = {"latitude": None, "longitude": None}
 
@@ -19,84 +23,100 @@ def calculate_distance(lat1, lon1, lat2, lon2):
 def calculate_travel_time(distance_km, speed_kmh=32):
     return (distance_km / speed_kmh) * 60
 
-vehicles = [
-    {"lat": 41.851637, "lon": -87.619049, "vid": "1362", "pdist": 20267},
-    {"lat": 41.811370, "lon": -87.616706, "vid": "8679", "pdist": 35053},
-    {"lat": 41.721882, "lon": -87.618286, "vid": "7921", "pdist": 68853}
-]
+class LaunchRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_request_type("LaunchRequest")(handler_input)
 
-# my_lat, my_lon = 41.85, -87.62
+    def handle(self, handler_input):
+        return handler_input.response_builder.speak("Welcome to the Bus Tracker. Ask me how far the bus is from you.").response
 
-@app.route("/", methods=["GET"])
-def home():
-    return "Welcome to Bus Tracker App! Use the Alexa interface to interact."
+class SetLocationIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("SetLocationIntent")(handler_input)
 
-@ask.launch
-def launch_skill():
-    return statement("Welcome to the Bus Tracker. Ask me how far the bus is from you.")
+    def handle(self, handler_input):
+        slots = handler_input.request_envelope.request.intent.slots
+        try:
+            lat_num = w2n.word_to_num(slots["lat"].value)
+            long_num = w2n.word_to_num(slots["long"].value)
+            user_location["latitude"] = lat_num
+            user_location["longitude"] = long_num
+            return handler_input.response_builder.speak(f"Your location is set to latitude {lat_num} and longitude {long_num}.").response
+        except ValueError:
+            return handler_input.response_builder.speak("I didn't understand the location. Please try again using numbers.").response
 
-@ask.intent("GetLocationIntent")
-def get_location():
-    device_id = session.context['System']['device']['deviceId']
-    api_access_token = session.context['System']['apiAccessToken']
-    api_endpoint = session.context['System']['apiEndpoint']
+class GetMyLocationIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("GetMyLocationIntent")(handler_input)
 
-    headers = {"Authorization": f"Bearer {api_access_token}"}
-    location_url = f"{api_endpoint}/v1/devices/{device_id}/settings/address"
-    response = requests.get(location_url, headers=headers)
+    def handle(self, handler_input):
+        lat = user_location.get("latitude")
+        lon = user_location.get("longitude")
 
-    if response.status_code == 200:
-        location_data = response.json()
-        latitude = location_data.get("latitude")
-        longitude = location_data.get("longitude")
-
-        if latitude and longitude:
-            user_location["latitude"] = latitude
-            user_location["longitude"] = longitude
-            return statement(f"Your location is set to latitude {latitude} and longitude {longitude}.")
+        if lat is None or lon is None:
+            speech_text = "I don't have your location. Please set it first."
         else:
-            return statement("I couldn't retrieve your location. Please check your permissions.")
-    elif response.status_code == 403:
-        return statement("Location permissions are not granted. Please enable location access in the Alexa app.")
-    else:
-        return statement("I couldn't fetch your location due to an error. Please try again later.")
+            speech_text = f"Your current latitude is {lat} and longitude is {lon}."
 
-@ask.intent("GetBusTimeIntent", convert={'routeNumber': int})
-def get_bus_time(routeNumber):
-    if not routeNumber:
-        return statement("Please specify the bus route number, like bus 3 or bus 10.")
+        return handler_input.response_builder.speak(speech_text).set_card(SimpleCard("Your Location", speech_text)).response
 
-    if not user_location["latitude"] or not user_location["longitude"]:
-        return statement("Please set your location first using the Get Location command.")
-    
-    my_lat, my_lon = user_location["latitude"], user_location["longitude"]
+class GetBusTimeIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("GetBusTimeIntent")(handler_input)
 
-    cta_api_key = "API_KEY"
-    cta_url = f"http://www.ctabustracker.com/bustime/api/v2/getvehicles?key={cta_api_key}&rt={routeNumber}&tmres=m&format=json"
-    response = requests.get(cta_url)
+    def handle(self, handler_input):
+        slots = handler_input.request_envelope.request.intent.slots
+        route_number = slots.get("routeNumber").value
 
-    if response.status_code != 200:
-        return statement("I couldn't fetch the bus information. Please try again later.")
+        if not route_number:
+            return handler_input.response_builder.speak("Please specify a bus route number.").response
 
-    vehicle_data = response.json().get("bustime-response", {}).get("vehicle", [])
-    if not vehicle_data:
-        return statement("No buses are currently running in this route.")
+        if not user_location["latitude"] or not user_location["longitude"]:
+            return handler_input.response_builder.speak("I don't have your location. Please set it first.").response
 
-    nearby_buses = []
-    for vehicle in vehicle_data:
-        bus_lat = float(vehicle["lat"])
-        bus_lon = float(vehicle["lon"])
-        distance = calculate_distance(my_lat, my_lon, bus_lat, bus_lon)
-        travel_time = calculate_travel_time(distance)
-        nearby_buses.append({"vid": vehicle["vid"], "time": travel_time})
+        my_lat, my_lon = user_location["latitude"], user_location["longitude"]
+        cta_api_key = "API_KEY"
+        cta_url = f"http://www.ctabustracker.com/bustime/api/v2/getvehicles?key={cta_api_key}&rt={route_number}&tmres=m&format=json"
+        response = requests.get(cta_url)
 
-    if nearby_buses:
-        nearby_buses.sort(key=lambda x: x["time"])
-        nearest_bus = nearby_buses[0]
-        response = f"Bus {nearby_buses[0]['vid']} is approximately {nearby_buses[0]['time']:.2f} minutes away."
-        return statement(response)
-    else:
-        return statement("No buses are near you at the moment.")
+        if response.status_code != 200:
+            return handler_input.response_builder.speak("I couldn't fetch bus data. Try again later.").response
+
+        vehicle_data = response.json().get("bustime-response", {}).get("vehicle", [])
+        if not vehicle_data:
+            return handler_input.response_builder.speak(f"No buses found for route {route_number}.").response
+
+        nearby_buses = []
+        for vehicle in vehicle_data:
+            bus_lat = float(vehicle["lat"])
+            bus_lon = float(vehicle["lon"])
+            distance = calculate_distance(my_lat, my_lon, bus_lat, bus_lon)
+            travel_time = calculate_travel_time(distance)
+            nearby_buses.append({"vid": vehicle["vid"], "time": travel_time})
+
+        if nearby_buses:
+            nearby_buses.sort(key=lambda x: x["time"])
+            nearest_bus = nearby_buses[0]
+            return handler_input.response_builder.speak(f"Bus {nearest_bus['vid']} is approximately {nearest_bus['time']:.2f} minutes away.").response
+        else:
+            return handler_input.response_builder.speak("No buses are near you right now.").response
+
+skill_builder.add_request_handler(LaunchRequestHandler())
+skill_builder.add_request_handler(SetLocationIntentHandler())
+skill_builder.add_request_handler(GetBusTimeIntentHandler())
+skill_builder.add_request_handler(GetMyLocationIntentHandler())
+
+skill_adapter = SkillAdapter(skill=skill_builder.create(), skill_id="amzn1.ask.skill.YOUR_SKILL_ID", app=app)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return "Welcome to Bus Tracker App! Use the Alexa interface to interact."
+    return "POST request received"
+
+@app.route("/alexa", methods=["POST"])
+def alexa_endpoint():
+    return skill_adapter.dispatch_request()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
